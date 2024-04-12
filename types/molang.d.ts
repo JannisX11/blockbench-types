c16c291aebdf34a723880c597d349a3ebc61fd3f
SnaveSutit <snavesutit@gmail.com>
2024-04-12 19:50:48 -0400

    Correct global_variables type

--- types/molang.d.ts
+++ types/molang.d.ts
@@ -1,4 +1,4 @@
 declare class Molang {
 	parse(expression: string | number, variables?: Record<string, number>): number
-	global_variables: Record<string, number | ((...args: number[]) => number)>
+	global_variables: Record<string, string | number | ((...args: number[]) => number)>
 }
