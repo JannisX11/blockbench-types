9dd25cb2ad230e9818347067b7baff2c4d9a7752
SnaveSutit <snavesutit@gmail.com>
2024-04-12 21:09:23 -0400

    MolangExpression

--- types/molang.d.ts
+++ types/molang.d.ts
@@ -2,3 +2,13 @@ declare class Molang {
 	parse(expression: string | number, variables?: Record<string, number>): number
 	global_variables: Record<string, string | number | ((...args: number[]) => number)>
 }
+
+declare interface MolangExpression {
+	animation: _Animation
+	animator: BoneAnimator
+	channel: string
+	key: string
+	kf: _Keyframe
+	type: string
+	value: string
+}
